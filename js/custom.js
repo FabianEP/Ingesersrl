1b3c75f8239a5360384b969d1025052115ea8ace
AR-IT01866$@globant.com <AR-IT01866$@globant.com>
2017-08-14 00:41:23 -0300

    Fixing Style issues

--- js/custom.js
+++ js/custom.js
@@ -23,7 +23,7 @@ jQuery(document).ready(function() {
                       menu = target;
                   $target = $(target);
                   $('html, body').stop().animate({
-                      'scrollTop': $target.offset().top+2
+                      'scrollTop': $target.offset().top-55
                   }, 500, 'swing', function () {
                       window.location.hash = target;
                       $(document).on("scroll", onScroll);
@@ -32,7 +32,7 @@ jQuery(document).ready(function() {
           });
 
           function onScroll(event){
-              var scrollPos = $(document).scrollTop();
+              var scrollPos = $(document).scrollTop() + 57;
               $('.navbar-default .navbar-nav>li>a').each(function () {
                   var currLink = $(this);
                   var refElement = $(currLink.attr("href"));
